7d34b5aaff2fba0ad6961e1cdd9636e3851bd583
Black-Tomcat <tomcat.frantz@gmail.com>
2022-07-26 21:23:27 +1000

    Add new main method for world sim

--- src/components/world/mod.rs
+++ src/components/world/mod.rs
@@ -1,1 +1,34 @@
+use bevy::prelude::*;
+use bevy_prototype_lyon::entity::ShapeBundle;
+use bevy_prototype_lyon::prelude::*;
+
 pub mod tectonics;
+
+const PIXEL_SIZE: f32 = 4.;
+
+fn get_tile(index: UVec2) -> ShapeBundle {
+    let city_shape = shapes::Rectangle {
+        extents: Vec2::new(PIXEL_SIZE, PIXEL_SIZE),
+        origin: RectangleOrigin::BottomLeft,
+    };
+
+    GeometryBuilder::build_as(
+        &city_shape,
+        DrawMode::Fill(FillMode::color(MapColour::Dark.into())),
+        Transform::from_xyz(index.x as f32, index.y as f32, ZIndex::City.into()),
+    )
+}
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "I am a window!".to_string(),
+            width: 360. * PIXEL_SIZE,
+            height: 180. * PIXEL_SIZE,
+            ..default()
+        })
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .run()
+}
